f0af5abd1511841e9fb20d66146b94334b3cbad5
mehedihasan605 <iammehedihasanrony@gmail.com>
2025-09-15 18:10:16 +0600

    initial setup done

---
+++ server/src/server.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client';
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+//load all your enviroment variables
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(cookieParser());
+
+export const prisma = new PrismaClient();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello from E-Commerce backend');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
+
+process.on('SIGINT', async () => {
+  await prisma.$disconnect();
+  process.exit();
+});
